1e6c3bbd29a217b5a71f0a884a17c171da85dd7e
acharbel54 <acharbeladonis@ept.sn>
2025-07-20 00:16:59 +0000

    estoy

--- src/test/java/sn/ept/git/seminaire/cicd/entities/TagTest.java
+++ src/test/java/sn/ept/git/seminaire/cicd/entities/TagTest.java
@@ -1,14 +1,20 @@
 package sn.ept.git.seminaire.cicd.entities;
 
 import org.junit.jupiter.api.Test;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TagTest {
     @Test
     void testGettersSettersEqualsHashCodeToString() {
+        LocalDateTime now = LocalDateTime.now(ZoneOffset.UTC);
+        
         Tag tag1 = new Tag();
         tag1.setId("id1");
         tag1.setName("name1");
+        tag1.setCreatedDate(now);
+        tag1.setLastModifiedDate(now);
         
         // Test getters/setters
         assertEquals("id1", tag1.getId());
@@ -17,6 +23,9 @@ class TagTest {
         Tag tag2 = new Tag();
         tag2.setId("id1");
         tag2.setName("name1");
+        tag2.setCreatedDate(now);
+        tag2.setLastModifiedDate(now);
+        
         assertEquals(tag1, tag2);
         assertEquals(tag1.hashCode(), tag2.hashCode());
         assertNotNull(tag1.toString());
